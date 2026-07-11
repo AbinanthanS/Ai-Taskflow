61e9a9f0ad61f37ca7e6f160cd4a937f24637a3a
Abinanthan <s.a.abinanthan2005@gmail.com>
2026-07-12 01:07:13 +0530

    implemented: error handling

---
+++ Backend/src/middleware/errorHandler.js
@@ -0,0 +1,15 @@
+const errorHandler = (err, _req,res,_next) => {
+    const status = err.statusCode || 500;
+
+    if (Status >= 500) console.error("Server error: ", err);
+    if (err.code === "23505") return res.status(409).json({error: "Resources already exists"});
+    res.status(status).json({
+        error: status >= 500 ? "Internet server error":err.message,
+    });
+};
+
+const notFoundHandler = (_req,ers) => {
+    res.status(404).json({error: "Route not found"});
+};
+
+module.exports = { errorHandler, notFoundHandler};
